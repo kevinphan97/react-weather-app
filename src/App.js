11de00b5e19dcbefd5fd094b79beb1017c38d671
Kevin Phan <kevin.b.phan97@gmail.com>
2022-08-06 17:25:33 -0700

    App Functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
 
-  //const url = `https://api.openweathermap.org/data/2.5/weather?q=phoenix&appid=b441dd6636d6c89557d63d18d5c4b85e`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=b441dd6636d6c89557d63d18d5c4b85e`
+
+  const searchLocation = (e) => {
+    if (e.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
 
   return (
-    <div className= 'app'>
+    <div className = 'app'>
+      <div className = 'search'>
+        <input 
+        type = 'text'
+        value = {location}
+        onChange = {e => setLocation(e.target.value)}
+        onKeyPress = {searchLocation} 
+        placeholder = 'Enter a Location'
+        />
+      </div>
       <div className = 'container'>
-        <div className= 'top'>
+        <div className = 'top'>
           <div className = 'location'>
             <p>Phoenix</p>
           </div>
@@ -19,7 +41,7 @@ function App() {
             <p>Sunny</p>
           </div>
         </div>
-        <div className= 'bottom'>
+        <div className = 'bottom'>
           <div className = 'feels'>
             <p className = 'bold'>105°F</p>
             <p>Feels like</p>
